347d666c31b6fadc52736b0704fe815275cdfba3
jjarcade <jesse.j.juarez@gmail.com>
2017-02-18 20:32:44 -0600

    done for saturday

--- Database/add_damage trigger.sql
+++ Database/add_damage trigger.sql
@@ -1,8 +1,8 @@
-CREATE TRIGGER add_damagelo AFTER INSERT ON temp_weapons
+CREATE TRIGGER add_damages AFTER INSERT ON weapon_builds
 BEGIN
-UPDATE temp_weapons set Damagelo=(SELECT Damagelo from Weapon_damages where weapon_id=new.weapon) where weapon=new.weapon;
-UPDATE temp_weapons set Element1=(SELECT Element1 from Weapon_damages where weapon_id=new.weapon) where weapon=new.weapon;
-UPDATE temp_weapons set Element1DamageLo=(SELECT Element1Damagelo from Weapon_damages where weapon_id=new.weapon) where weapon=new.weapon;
-UPDATE temp_weapons set Element2=(SELECT Element2 from Weapon_damages where weapon_id=new.weapon) where weapon=new.weapon;
-UPDATE temp_weapons set Element2DamageLo=(SELECT Element2Damagelo from Weapon_damages where weapon_id=new.weapon) where weapon=new.weapon;
+UPDATE weapon_builds set Damagelo=(SELECT Damagelo from Weapon_damages where weapon_id=new.weapon) where weapon=new.weapon;
+UPDATE weapon_builds set Element1=(SELECT Element1 from Weapon_damages where weapon_id=new.weapon) where weapon=new.weapon;
+UPDATE weapon_builds set Element1DamageLo=(SELECT Element1Damagelo from Weapon_damages where weapon_id=new.weapon) where weapon=new.weapon;
+UPDATE weapon_builds set Element2=(SELECT Element2 from Weapon_damages where weapon_id=new.weapon) where weapon=new.weapon;
+UPDATE weapon_builds set Element2DamageLo=(SELECT Element2Damagelo from Weapon_damages where weapon_id=new.weapon) where weapon=new.weapon;
 END;
