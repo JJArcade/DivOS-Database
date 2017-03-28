402f13ed92667048e6cb904d5fdd482f59db02f0
jess <jesse.j.juarez@gmail.com>
2017-03-27 22:04:33 -0500

    test

--- Database/Gui_test.py
+++ Database/Gui_test.py
@@ -5,7 +5,9 @@ pygame.init()
 screen_size = (700,900)
 DISPLAYSURF = pygame.display.set_mode(screen_size)
 pygame.display.set_caption('Hello World')
-while True: #main game loop
+
+#Main game loop
+while True:
     for event in pygame.event.get():
         print(event)
         if event.type == QUIT:
