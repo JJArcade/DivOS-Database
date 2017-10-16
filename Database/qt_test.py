a374c0640b3ec5c1152bab669e9c996907f36a51
jjarcade <jesse.j.juarez@gmail.com>
2017-10-16 17:38:41 -0400

    end of day

--- Database/qt_test.py
+++ Database/qt_test.py
@@ -7,16 +7,41 @@ class Example(QWidget):
     
     def __init__(self):
         super().__init__()
-        self.div = divsqlite("Divinity.db")
+        #self.div = divsqlite("Divinity.db")
+        self.initial()
         
     def initial(self):
+        btn1 = QPushButton(("Armor Builds"), self)
+        btn1.move(30,30)
+        btn1.clicked.connect(self.open_table)
+
+        self.show()
+
+    def open_table(self):
+        sender = self.sender()
+        ab_s = armor_build_window()
+        ab_s.show()
+
+
+
+class armor_build_window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.div = divsqlite("Divinity.db")
+        self.initial()
+
+    def initial(self):
+        self.armor_table()
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.ab_table)
+        #self.show()
 
     def armor_table(self):
         self.ab_table = QTableWidget()
         
         #get row and column counts
         self.div.curr.execute("SELECT count(*) FROM armor_builds")
-        ab_rows = int(self.curr.fetchall()[0][0])
+        ab_rows = int(self.div.curr.fetchall()[0][0])
         ab_columns = 5  #helmet, boots, chest and gloves with armor rating
         self.ab_table.setRowCount(ab_rows)
         self.ab_table.setColumnCount(ab_columns)
@@ -32,10 +57,8 @@ class Example(QWidget):
                     a_name = QTableWidgetItem(a_name)
                     self.ab_table.setItem(a,b,a_name)
                 else:
-                    a_rating = QTableWidgetItem(sets[a][b])
-
-        
-        
+                    a_rating = QTableWidgetItem(str(sets[a][b]))
+                    self.ab_table.setItem(a,b,a_rating)
         
 if __name__ == '__main__':
     
